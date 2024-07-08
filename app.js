f6f85de8140070663a1cf1337cf728c1644b2d41
Bipon Roy <biponroy400@gmail.com>
2024-07-08 16:41:20 +0600

    added hero section

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const dropdownOpenBtn = document.getElementById("dropdown-open");
-    const dropdownCloseBtn = document.getElementById("dropdown-close");
-    const navDropdown = document.getElementById("nav-dropdown");
+const dropdownOpenBtn = document.getElementById("dropdown-open");
+const dropdownCloseBtn = document.getElementById("dropdown-close");
+const navDropdown = document.getElementById("nav-dropdown");
 
-    dropdownOpenBtn.addEventListener("click", function () {
-        navDropdown.classList.add("show");
-    });
+dropdownOpenBtn.addEventListener("click", () => {
+    navDropdown.classList.add("show");
+});
 
-    dropdownCloseBtn.addEventListener("click", function () {
-        navDropdown.classList.remove("show");
-    });
+dropdownCloseBtn.addEventListener("click", () => {
+    navDropdown.classList.remove("show");
 });
