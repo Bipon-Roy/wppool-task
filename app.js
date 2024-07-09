72b0442ec89b7153ff91052d02e1c66d73044715
Bipon Roy <biponroy400@gmail.com>
2024-07-09 13:53:26 +0600

    added sticky nav style

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// navbar menu section
 const dropdownOpenBtn = document.getElementById("dropdown-open");
 const dropdownCloseBtn = document.getElementById("dropdown-close");
 const navDropdown = document.getElementById("nav-dropdown");
@@ -9,3 +10,17 @@ dropdownOpenBtn.addEventListener("click", () => {
 dropdownCloseBtn.addEventListener("click", () => {
     navDropdown.classList.remove("show");
 });
+//end of navbar menu section
+
+// sticky navbar
+window.addEventListener("scroll", function () {
+    const navbar = document.getElementById("navbar");
+
+    if (window.scrollY > 100) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+});
+
+//end of sticky navbar
