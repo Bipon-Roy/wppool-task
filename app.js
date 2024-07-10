725fbbe24afbc7ec6b17ea96c4b63d01a93b04d1
Bipon Roy <biponroy400@gmail.com>
2024-07-11 01:52:16 +0600

    added responsive design

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ dropdownCloseBtn.addEventListener("click", () => {
 window.addEventListener("scroll", function () {
     const navbar = document.getElementById("navbar");
 
-    if (window.scrollY > 100) {
+    if (window.scrollY > 50) {
         navbar.classList.add("sticky");
     } else {
         navbar.classList.remove("sticky");
